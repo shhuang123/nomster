c8f7e77c8e4367360fb368d895cc7a9acca99a71
shhuang123 <sharonhuang923@gmail.com>
2019-02-03 07:55:54 +0000

    add destory permisson

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destory]
 
   def index
     @places = Place.all.paginate(page: params[:page], per_page: 10)
@@ -30,6 +30,7 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user !=current_user
       return render plain: 'Not Allowed', status: :forbidden
+    end
 
     @place.update_attributes(place_params)
     redirect_to root_path
@@ -37,6 +38,10 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user !=current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
   end 
