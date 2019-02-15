7d2b3b42b752d504a3e9fcf032564935466ecc55
shhuang123 <sharonhuang923@gmail.com>
2019-02-15 02:17:27 +0000

    set up comment database/model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
