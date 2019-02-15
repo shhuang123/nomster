82d2ad90e59eb7de7b0de9da1f925bab2a10f57f
shhuang123 <sharonhuang923@gmail.com>
2019-02-15 03:18:25 +0000

    comment submission form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places
-end
+  resources :places do
+    resources :comments, only: :create
+  end
+end 
+ 
