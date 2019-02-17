817221c22de5fb27deeebadffa096051835bdb0a
shhuang123 <sharonhuang923@gmail.com>
2019-02-17 05:24:10 +0000

    adding photos to the app

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
-  
+  has_many :photos
+
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
